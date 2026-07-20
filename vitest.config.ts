4d40556856c0a144730d15c806240905e6736f35
lbarros <lbarros@tecgraf.puc-rio.br>
2026-07-20 16:01:07 -0300

    Migração para nova aplicação Angular

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    environment: 'jsdom',
-    include: ['test/src/**/*.spec.ts'],
-    setupFiles: ['test/setup.ts'],
-  },
-});
