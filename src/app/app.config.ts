8bf40a696ce9dc3b5082290424071428a5c6a7a1
lbarros <lbarros@tecgraf.puc-rio.br>
2026-07-21 14:21:33 -0300

    Interceptador para mockar a resposta do servidor em modo dev

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { cadastroMockInterceptor } from './auth/cadastro/cadastro-mock.interceptor';
+
+const httpProvider = isDevMode() ? provideHttpClient(withInterceptors([cadastroMockInterceptor])) : provideHttpClient();
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(),
+    httpProvider,
   ]
 };
